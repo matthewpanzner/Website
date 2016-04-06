9487748db14ddfe7fc1570f9f240b409b346b887
panzner.matthew <panzner.matthew@gmail.com>
2016-04-06 16:52:44 -0400

    Formatted Contact code
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- include/view/contact.php
+++ include/view/contact.php
@@ -1,28 +1,28 @@
-<!--This is practing the grid CSS that I made-->
-<section class="container">
-  <!--Create a grid-->
-  <div class="grid grid-1-2 grid--center">
-    <!--Create a cell in that grid, this grid is one cell-->
-    <div class="grid_cell">
-      <!--Add content to the cell, the content will be another grid-->
-      <div class="grid_cell--content">
-        <!--Add grid as content, this will have two cells-->
-        <div class="grid grid-1-2">
-          <!--Add a cell to the grid inside a grid--> 
+      <!--This is practing the grid CSS that I made-->
+      <section class="container">
+        <!--Create a grid-->
+        <div class="grid grid-1-2 grid--center">
+          <!--Create a cell in that grid, this grid is one cell-->
           <div class="grid_cell">
-            <div class="grid_cell--content" style="background-color:grey">
-              <h1>IAMERROR</h1>
-              <h4>xxxx@xxxx.com</h4>
-              <h4>x-xxx-xxx-xxxx</h4>
-            </div>
-          </div>
-          <div class="grid_cell">
-            <div class="grid_cell--content" style="background-color:blue">
-              Test
+            <!--Add content to the cell, the content will be another grid-->
+            <div class="grid_cell--content">
+              <!--Add grid as content, this will have two cells-->
+              <div class="grid grid-1-2">
+                <!--Add a cell to the grid inside a grid--> 
+                <div class="grid_cell">
+                  <div class="grid_cell--content" style="background-color:grey">
+                    <h1>IAMERROR</h1>
+                    <h4>xxxx@xxxx.com</h4>
+                    <h4>x-xxx-xxx-xxxx</h4>
+                  </div>
+                </div>
+                <div class="grid_cell">
+                  <div class="grid_cell--content" style="background-color:blue">
+                    Test
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
+      </section>
