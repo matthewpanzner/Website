90da4f5ea8bd3c66a0ea3d9da7e2b695e0c1dc22
panzner.matthew <panzner.matthew@gmail.com>
2016-04-06 18:11:29 -0400

    Added to DB Script
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- scripts/SQL/Database/build.sql
+++ scripts/SQL/Database/build.sql
@@ -1,8 +1,18 @@
-CREATE DATABASE IF NOT EXIST Website;
+DROP DATABASE IF EXISTS Website;
+CREATE DATABASE Website;
 
 use Website;
 
-CREATE TABLE `Users` (
+DROP TABLE IF EXISTS PrivilegeRole;
+CREATE TABLE PrivilegeRole (
+ `roleId` int(15) NOT NULL AUTO_INCREMENT,
+ `role` varchar(31) NOT NULL,
+ PRIMARY KEY (`roleId`),
+ UNIQUE KEY `role` (`role`)
+);
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
  `userId` int(15) NOT NULL AUTO_INCREMENT,
  `username` varchar(65) NOT NULL,
  `password` varchar(65) NOT NULL,
@@ -16,14 +26,16 @@ CREATE TABLE `Users` (
  CONSTRAINT `fk_constraint_User_On_Role` FOREIGN KEY (`roleId`) REFERENCES `PrivilegeRole` (`roleId`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE `ArticleCategory` (
+DROP TABLE IF EXISTS ArticleCategory;
+CREATE TABLE ArticleCategory (
  `id` int(5) NOT NULL AUTO_INCREMENT,
  `name` varchar(255) NOT NULL,
  `summary` text,
  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `Articles` (
+DROP TABLE IF EXISTS Articles;
+CREATE TABLE Articles (
  `articleId` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
  `publicationDate` date DEFAULT NULL,
  `title` varchar(255) NOT NULL,
@@ -33,12 +45,5 @@ CREATE TABLE IF NOT EXISTS `Articles` (
  PRIMARY KEY (`articleId`),
  KEY `category` (`category`),
  CONSTRAINT `fk_constraint_categoryId` FOREIGN KEY (`category`) REFERENCES `ArticleCategory` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS `PrivilegeRole` (
- `roleId` int(15) NOT NULL AUTO_INCREMENT,
- `role` varchar(31) NOT NULL,
- PRIMARY KEY (`roleId`),
- UNIQUE KEY `role` (`role`)
 );
 
