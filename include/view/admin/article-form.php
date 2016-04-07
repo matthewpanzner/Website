d4f53f4b92eb435447644f65821213a818d58d16
panzner.matthew <panzner.matthew@gmail.com>
2016-04-07 14:03:45 -0400

    Put Required Field
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- include/view/admin/article-form.php
+++ include/view/admin/article-form.php
@@ -6,7 +6,7 @@
           <textarea rows="20" cols="64" type="text" name="content" placeholder="content" required></textarea>
           <input type="text" name="publication-date" value=<?php echo date("Y-m-d")?> readonly required/>
 <?php
-$html = "    <select name='category'>\n";
+$html = "    <select name='category' required>\n";
 if(mysqli_num_rows($this->model['categories'])){
   while($row = mysqli_fetch_row($this->model['categories'])){
       $html .= "      <option value='" . $row[0] . "'>" . $row[1] . "</option>\n";
--- scripts/SQL/TestScripts/dbReset.sql
+++ scripts/SQL/TestScripts/dbReset.sql
@@ -1,18 +1,3 @@
-INSERT INTO ArticleCategory (name, summary) VALUES ("Main_Test_001", "Testing");
-INSERT INTO ArticleCategory (name, summary) VALUES ("Math", "Mathematical stuff");
-INSERT INTO ArticleCategory (name, summary) VALUES ("Programming", "C++ and more");
-INSERT INTO ArticleCategory (name, summary) VALUES ("Games", "About games and stuff");
-
-INSERT INTO Articles (title, summary, content, category) VALUES ("Game1", "xxx", "zzz", 4);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Game2", "xxx", "zzz", 4);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Math1", "xxx", "zzz", 2);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Math2", "xxx", "zzz", 2);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Math3", "xxx", "zzz", 2);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Test1", "xxx", "zzz", 1);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Test2", "<a href='#'>encode/decode</a>", "zzz", 1);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Test3", "ovveerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrflow", "zzz", 1);
-INSERT INTO Articles (title, summary, content, category) VALUES ("Test4", "xxx", "<p>This</p><p>Tests</p><p>Auto</p><p>Sizing</p><p>!</p>", 1);
-
 INSERT INTO PrivilegeRole VALUES (1, 'user');
 INSERT INTO PrivilegeRole VALUES (2, 'admin');
 
