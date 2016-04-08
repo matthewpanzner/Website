0a7a1dc664c89e6278c76cbe1218d7297dd2ffcf
panzner.matthew <panzner.matthew@gmail.com>
2016-04-08 17:00:04 -0400

    Changed footer
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- include/templates/frontend/footer.php
+++ include/templates/frontend/footer.php
@@ -6,10 +6,6 @@
         <li><a href="index.php?route=registration">Register</a></li>
 <?php else: ?>
         <li><a href="index.php?controller=LoginController&action=onLogout">Logout</a></li>
-<?php if(isset($role) && $role==="admin"): ?>
-        <li><a href="index.php?controller=ArticleFormController&action=onLoad">Add Article</a></li>
-        <li><a href="index.php?route=admin/article-category-form">Add Article Category</a></li>
-<?php endif;?>
 <?php endif;?>
       </ul>
     </footer>
