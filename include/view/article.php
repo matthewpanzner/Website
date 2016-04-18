f673b8ffd435dbdd4773d56dc2cc33381ab417cc
panzner.matthew <panzner.matthew@gmail.com>
2016-04-18 10:11:49 -0400

    Updated CSS
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- include/view/article.php
+++ include/view/article.php
@@ -1,7 +1,8 @@
-     <section class="article">
+      <article class="article">
         <div class="content">
           <h1 id="article-title"><?php echo $this->model['article']->title;?></h1>
+          <hr>
 <?php echo formatHtml(htmlspecialchars_decode($this->model['article']->content), 10);?>
           <h5><?php echo $this->model['article']->publicationDate;?></h5>
         </div>
-      </section>
+      </article>
