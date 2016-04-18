75c8781c3c657e19a0d4b94840067a24cfcfdcad
panzner.matthew <panzner.matthew@gmail.com>
2016-04-18 16:16:27 -0400

    CSS Tuning
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- include/view/article.php
+++ include/view/article.php
@@ -1,7 +1,8 @@
       <article class="article">
+        <div id="article-title">
+          <h1><?php echo $this->model['article']->title;?></h1>
+        </div>
         <div class="content">
-          <h1 id="article-title"><?php echo $this->model['article']->title;?></h1>
-          <hr>
 <?php echo formatHtml(htmlspecialchars_decode($this->model['article']->content), 10);?>
           <h5><?php echo $this->model['article']->publicationDate;?></h5>
         </div>
