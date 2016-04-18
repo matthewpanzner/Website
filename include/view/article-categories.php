4abc59e0499d413e49402734ca6888e5e7dbbc05
panzner.matthew <panzner.matthew@gmail.com>
2016-04-18 11:53:50 -0400

    Added CSS For Article Grid
    Signed-off-by: panzner.matthew <panzner.matthew@gmail.com>

--- include/view/article-categories.php
+++ include/view/article-categories.php
@@ -2,22 +2,27 @@
 $html = "<div class='grid grid-1-2'>\n";
 if(mysqli_num_rows($this->model['categories'])){
   while($row = mysqli_fetch_row($this->model['categories'])){
-    $html .= "    <div class='grid_cell'>\n";
-    $html .= "      <div class='grid_cell--content article_cell grid-shadow'>\n";
+    $html .= "  <div class='grid_cell'>\n";
+    $html .= "    <div class='grid_cell--content article_cell grid-shadow'>\n";
    // $html .= "    <td><a href=index.php?controller=ArticleController&action=getArticle&id=0' style='display:block'>&nbsp;</a></td>\n";
     foreach($row as $key=>$value){
-      if($key == 1)
-        $html .= "        <a href='index.php?controller=ArticleController&action=onGetArticlesByCategory&c=" . $row[0] . "'><h1>" . $value . "</h1>\n";
+      if($key == 1){
+        $html .= "      <div class='article_cell-title'>\n";
+        $html .= "        <a href='index.php?controller=ArticleController&action=onGetArticlesByCategory&c=" . $row[0] . "'><h1>" . $value . "</h1></a>\n";
+        $html .= "      </div>\n";
+        $html .= "      <div class='article_cell-body'>\n";
+      }
       if($key == 2)
-        $html .= "          <p>" . $row[2] . "</p>\n        </a>\n";
+        $html .= "        <p>" . $row[2] . "</p>\n";
     }
     
     if($role == 'admin'){
-      $html .= "        <a id='article-delete' href='index.php?controller=ArticleCategoryController&action=onDelete&id=" . $row[0] . "'>delete</a>\n";
-      $html .= "        <a id='article-edit' href='#'>edit</a>\n";
+      $html .= "          <a id='article-delete' href='index.php?controller=ArticleCategoryController&action=onDelete&id=" . $row[0] . "'>delete</a>\n";
+      $html .= "          <a id='article-edit' href='#'>edit</a>\n";
     }
     
-    $html .= "     </div>\n";
+    $html .= "      </div>\n";
+    $html .= "    </div>\n";
     $html .= "  </div>\n";
   }
 }
--- include/view/articles.php
+++ include/view/articles.php
@@ -1,23 +1,30 @@
 <?php 
-$html = "  <div class='grid grid-1-3'>\n";
+$html = "<div class='grid grid-1-3'>\n";
 if(mysqli_num_rows($this->model['articles'])){ //Needs to be changed to do in controller
   while($row = mysqli_fetch_row($this->model['articles'])){
-    $html .= "    <div class='grid_cell'>\n";
-    $html .= "      <div class='grid_cell--content article_cell grid-shadow'>\n";
+    $html .= "  <div class='grid_cell'>\n";
+    $html .= "    <div class='grid_cell--content article_cell grid-shadow'>\n";
     
     foreach($row as $key=>$value){
 
-      if($key === 1)
-        $html .= "      <a href='index.php?controller=ArticleController&action=onGetArticle&id=" . $row[0] . "'><h1>" . htmlspecialchars_decode($value) . " </h1></a>\n";
-      else if($key === 2  || $key === 4)
-        $html .= "      <p>" . htmlspecialchars_decode($value) . "</p>\n";
+      if($key === 1){ //title
+        $html .= "      <div class='article_cell-title'>\n";
+        $html .= "        <a href='index.php?controller=ArticleController&action=onGetArticle&id=" . $row[0] . "'><h1>" . htmlspecialchars_decode($value) . " </h1></a>\n";
+        $html .= "      </div>\n";
+        $html .="       <div class='article_cell-body'>\n";
+      }
+      else if($key === 2) //summary
+        $html .= "        <p>" . htmlspecialchars_decode($value) . "</p>\n";
+      else if($key == 4) //date
+        $html .= "        <span class='article-date'>" . htmlspecialchars_decode($value) . "</span>\n"; 
     }
     
     if($role == 'admin'){
-      $html .= "        <a id='article-delete' href='index.php?controller=ArticleController&action=onDelete&id=" . $row[0] . "'>delete</a>\n";
-      $html .= "        <a id='article-edit' href='index.php?controller=ArticleFormController&action=onLoad&id=" . $row[0] . "'>edit</a>\n";
+      $html .= "          <a id='article-delete' href='index.php?controller=ArticleController&action=onDelete&id=" . $row[0] . "'>delete</a>\n";
+      $html .= "          <a id='article-edit' href='index.php?controller=ArticleFormController&action=onLoad&id=" . $row[0] . "'>edit</a>\n";
     }
     
+    $html .= "      </div>\n";
     $html .= "    </div>\n";
     $html .= "  </div>\n";
   }
@@ -35,4 +42,4 @@ if($role == 'admin'){
 
 $html .= "</div>";
 
-echo formatHtml($html, 4);?>
+echo formatHtml($html, 6);?>
